057e2c649deeb677c0b72685555ea04a1612941e
teko teko <tekosolution23@gmail.com>
2023-12-11 13:46:19 +0100

    add complex  animation on comments

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Comment } from '../../../core/models/comment.model';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, state, style, transition, trigger, query } from '@angular/animations';
 
 /**
  * @triger déclencheur d'annimation qui va permettre d'implémenter une annimation 
@@ -29,7 +29,29 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
       ]),
       transition('active => default', [
         animate('500ms ease-in-out'),
-      ])
+      ]),
+      transition('void => *', [
+        query('span', [
+          style({
+            opacity: 0
+          }),
+        ]),
+        style({
+          transform: 'translateX(-100%)',
+          opacity: 0,
+          'background-color': 'rgb(201, 157, 242)'
+        }),
+        animate('250ms ease-out', style({
+          transform: 'translateX(0)',
+          opacity: 1,
+          'background-color': 'white',
+        })),
+        query('span', [
+          style({
+            opacity: 1
+          }),
+        ])
+      ]),
     ])
   ]
 })
@@ -64,6 +86,13 @@ export class CommentsComponent implements OnInit {
     if (this.commentCtrl.invalid) {
       return;
     }
+    const maxId = Math.max(... this.comments.map(comment => comment.id))
+    this.comments.unshift({
+      id: maxId + 1,
+      comment: this.commentCtrl.value,
+      createdDate: new Date().toISOString(),
+      userId: 1
+    });
     this.newComment.emit(this.commentCtrl.value);
     this.commentCtrl.reset();
   }
